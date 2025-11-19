2efa9da6c7253a789fa50379b0fa6ee2c552c9e5
Talla Sowmya Goud <sowmyagoud0219@gmail.com>
2025-11-19 20:05:54 +0100

    Add Driver class and create_drivers logic

---
+++ simulation/driver.py
@@ -0,0 +1,6 @@
+class Driver:
+    def __init__(self, driver_id, speed_factor, region, break_frequency):
+        self.driver_id = driver_id
+        self.speed_factor = speed_factor
+        self.region = region
+        self.break_frequency = break_frequency
--- simulation/engine.py
+++ simulation/engine.py
@@ -1,3 +1,5 @@
+import random
+
 class SimulationEngine:
     def __init__(self):
         self.current_time = 0
@@ -6,7 +8,15 @@ class SimulationEngine:
         self.logs = []
 
     def create_drivers(self):
-        pass
-    
+        number_of_drivers = random.randint(5, 10)
+        for i in range(number_of_drivers):
+            i += 1
+            speed_factor = random.uniform(0.8, 1.2)
+            region = random.choice(["city_center", "suburbs", "industrial", "rural"])
+            break_frequency = random.choice(["high", "low", "medium"])
+            driver = Driver(i, speed_factor, region, break_frequency)
+            self.drivers.append(driver)
+
+
     def assign_delivery(self):
         pass
