af3a0095f5bf99f9c15ed5d9f16d44803cc20652
Talla Sowmya Goud <sowmyagoud0219@gmail.com>
2025-11-19 19:14:49 +0100

    Engine Skeleton Added

---
+++ simulation/engine.py
@@ -0,0 +1,12 @@
+class SimulationEngine:
+    def __init__(self):
+        self.current_time = 0
+        self.drivers = []
+        self.deliveries = []
+        self.logs = []
+
+    def create_drivers(self):
+        pass
+    
+    def assign_delivery(self):
+        pass
