09e33cf15b3e08bfeea9565b8b3f80f59dc28dfb
Talla Sowmya Goud <sowmyagoud0219@gmail.com>
2025-11-19 21:38:50 +0100

    Add Delivery class and assign_deliveries logic

---
+++ simulation/delivery.py
@@ -0,0 +1,6 @@
+class Delivery:
+    def __init__(self, delivery_id, driver, route_type, distance):
+        self.delivery_id = delivery_id
+        self.driver = driver
+        self.route_type = route_type
+        self.distance = distance
--- simulation/engine.py
+++ simulation/engine.py
@@ -1,4 +1,7 @@
+from .delivery import Delivery
+from .driver import Driver
 import random
+
 
 class SimulationEngine:
     def __init__(self):
@@ -7,10 +10,10 @@ class SimulationEngine:
         self.deliveries = []
         self.logs = []
 
+    
     def create_drivers(self):
         number_of_drivers = random.randint(5, 10)
         for i in range(number_of_drivers):
-            i += 1
             speed_factor = random.uniform(0.8, 1.2)
             region = random.choice(["city_center", "suburbs", "industrial", "rural"])
             break_frequency = random.choice(["high", "low", "medium"])
@@ -19,4 +22,16 @@ class SimulationEngine:
 
 
     def assign_delivery(self):
-        pass
+        delivery_id = len(self.deliveries) + 1
+        driver = random.choice(self.drivers)
+        route_type = random.choice(["city_center", "suburbs", "industrial", "rural"])
+        if route_type == "city_center":
+            distance = random.uniform(2, 8)
+        elif route_type == "suburbs":
+            distance = random.uniform(7, 20)
+        elif route_type == "industrial":
+            distance = random.uniform(5, 15)
+        else:
+            distance = random.uniform(10, 30)
+        delivery = Delivery(delivery_id, driver, route_type, distance)
+        self.deliveries.append(delivery)  
