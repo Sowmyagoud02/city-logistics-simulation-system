3ca4c8c1bb74b0d8f825fa65e30c4551a0ef257c
Talla Sowmya Goud <sowmyagoud0219@gmail.com>
2025-11-20 20:49:02 +0100

    Added Delay and Simulation clock logic

--- simulation/delivery.py
+++ simulation/delivery.py
@@ -4,3 +4,9 @@ class Delivery:
         self.driver = driver
         self.route_type = route_type
         self.distance = distance
+        self.start_time = None
+        self.end_time = None
+        self.delay_reason = None
+        self.delay_minutes = None
+        self.status = "completed"
+        self.travel_time_minutes = 0
--- simulation/engine.py
+++ simulation/engine.py
@@ -1,5 +1,5 @@
-from .delivery import Delivery
-from .driver import Driver
+from delivery import Delivery
+from driver import Driver
 import random
 
 
@@ -9,7 +9,6 @@ class SimulationEngine:
         self.drivers = []
         self.deliveries = []
         self.logs = []
-
     
     def create_drivers(self):
         number_of_drivers = random.randint(5, 10)
@@ -19,11 +18,15 @@ class SimulationEngine:
             break_frequency = random.choice(["high", "low", "medium"])
             driver = Driver(i, speed_factor, region, break_frequency)
             self.drivers.append(driver)
+        # print(self.drivers)
 
 
     def assign_delivery(self):
         delivery_id = len(self.deliveries) + 1
         driver = random.choice(self.drivers)
+        # print(driver)
+        # print(driver.__dict__)
+
         route_type = random.choice(["city_center", "suburbs", "industrial", "rural"])
         if route_type == "city_center":
             distance = random.uniform(2, 8)
@@ -34,4 +37,35 @@ class SimulationEngine:
         else:
             distance = random.uniform(10, 30)
         delivery = Delivery(delivery_id, driver, route_type, distance)
-        self.deliveries.append(delivery)  
+
+        self.deliveries.append(delivery)
+
+        delivery.start_time = self.current_time
+
+        #base travel time
+        base_speed = 40
+        # print(driver.speed_factor, "hi")
+        # calculate travel time mins
+        effective_speed =  driver.speed_factor * base_speed
+        travel_time_hrs = distance / effective_speed
+        delivery.travel_time_minutes = travel_time_hrs * 60
+
+        #calculate delay mins
+        delay_choice = random.random()
+        if delay_choice < 0.3:
+            delivery.delay_reason = random.choice(["traffic", "break", "weather", "roadblock"])
+            delivery.delay_minutes = random.randint(5,25)
+        else:
+            delivery.delay_reason = None
+            delivery.delay_minutes = 0
+
+        #compute total time
+        delivery.end_time = delivery.start_time + delivery.travel_time_minutes + delivery.delay_minutes
+        
+        # update simulation clock
+        self.current_time = delivery.end_time
+
+
+obj = SimulationEngine()
+obj.create_drivers()
+obj.assign_delivery()
